c5ccf9a8183d6d8d73b272d3c46afa4a1d3bcf46
Vivyy <21315274+enginelesscc@users.noreply.github.com>
2026-07-13 15:42:41 +0200

    Add ExperimentDefaults

---
+++ plugins/ExperimentDefaults/src/index.tsx
@@ -0,0 +1,34 @@
+import { findByProps } from "@vendetta/metro";
+import { instead } from "@vendetta/patcher";
+
+function experimentOverride(args: any[], orig: Function) {
+	const [experimentId] = args;
+	if (experimentId === '2023-01_global_display_names' || 
+		experimentId === '2022-01_pronouns' || 
+		experimentId === '2023-01_silent_messages' || 
+		experimentId === '2021-09_favorites_server') {
+		return { type: 1, revision: 1, population: 0, override: true, bucket: 1 };
+	}
+	return orig(...args);
+}
+
+const patches: Array<() => void> = [];
+export default {
+    onLoad() {
+		const prop = findByProps("getExperimentDescriptor");
+		const userProp = findByProps("getUserExperimentDescriptor");
+		
+		patches.push(instead("getUserExperimentDescriptor", userProp, experimentOverride));
+		patches.push(instead("getExperimentDescriptor", prop, experimentOverride));
+		
+		console.log("[ExperimentDefaults]: Ready!");
+    },
+
+    onUnload() {
+		for (const p of patches) {
+            try { p(); } catch (e) { console.warn("[ExperimentDefaults]: failed to unpatch", e); }
+        }
+        patches.length = 0;
+		console.log("[ExperimentDefaults]: Unloaded!");
+	},
+};
---
+++ plugins/ExperimentDefaults/src/settings.tsx
@@ -0,0 +1,3 @@
+export default function FixDisplayNameSettings() {
+    return null;
+}
